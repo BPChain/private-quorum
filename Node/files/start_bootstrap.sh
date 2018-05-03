4493549dca500dd2817940bd6c8ba02d3068b486
Finn K <finn-klessascheck@o2mail.de>
2018-05-03 15:37:06 +0200

    Some fixes that still don't really work

--- Node/files/start_bootstrap.sh
+++ Node/files/start_bootstrap.sh
@@ -4,4 +4,4 @@ cd /root/files || exit
 ./geth --datadir=~/.ethereum/devchain --password <(echo -n 123)  account new
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
-./geth $GETH_OPTS
+./bootnode $GETH_OPTS
